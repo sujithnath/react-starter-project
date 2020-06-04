9e1b99ae879a04f9361ca2b41bb2f0f20b0a433a
sujithnath <sujithgnth@gmail.com>
2020-06-04 11:01:28 +0530

    webpack changes

--- webpack/webpack.base.js
+++ webpack/webpack.base.js
@@ -34,53 +34,6 @@ module.exports = {
         use: "file-loader",
       },
       { test: /\.json$/, loader: "json-loader" },
-      {
-        test: /\.(jpg|png|gif)$/,
-        use: [
-          {
-            loader: "url-loader",
-            options: {
-              // Inline files smaller than 10 kB
-              limit: 10 * 1024,
-            },
-          },
-          {
-            loader: "image-webpack-loader",
-            options: {
-              mozjpeg: {
-                enabled: false,
-                // NOTE: mozjpeg is disabled as it causes errors in some Linux environments
-                // Try enabling it in your environment by switching the config to:
-                // enabled: true,
-                // progressive: true,
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-              optipng: {
-                optimizationLevel: 7,
-              },
-              pngquant: {
-                quality: "65-90",
-                speed: 4,
-              },
-            },
-          },
-        ],
-      },
-      {
-        test: /\.html$/,
-        use: "html-loader",
-      },
-      {
-        test: /\.(mp4|webm)$/,
-        use: {
-          loader: "url-loader",
-          options: {
-            limit: 10000,
-          },
-        },
-      },
     ],
   },
   plugins: [
